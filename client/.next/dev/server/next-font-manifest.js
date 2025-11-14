0f20c034af0fef60fdc51c1a47c0239132d2c4b3
supreme2580 <victoromorogbe69@gmail.com>
2025-11-14 15:10:37 +0100

    Remove unused lock file and update postcss.js and build-manifest.json with new module paths. Fix module resolution errors in various components and update chunk references in static files.

--- client/.next/dev/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\n  \"app\": {\n    \"[project]/app/page\": [\n      \"static/media/0acc7fdf55eb3220-s.p.532ccaa1.woff2\"\n    ]\n  },\n  \"appUsingSizeAdjust\": true,\n  \"pages\": {},\n  \"pagesUsingSizeAdjust\": false\n}"
