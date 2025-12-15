aa1f123e7ada6baed0ddf84dcbed0204bedb59d4
Tony Stark <oktonystark8@gmail.com>
2025-12-15 14:10:04 -0600

    cartridge policy fix

--- client/app/providers/starknet-provider.tsx
+++ client/app/providers/starknet-provider.tsx
@@ -1,10 +1,26 @@
 import React from "react";
- 
+
 import { sepolia, mainnet } from "@starknet-react/chains";
-import { StarknetConfig, jsonRpcProvider, braavos, argent, voyager } from "@starknet-react/core";
+import {
+  StarknetConfig,
+  jsonRpcProvider,
+  braavos,
+  argent,
+  voyager,
+} from "@starknet-react/core";
 
 import { ControllerConnector } from "@cartridge/connector";
-import { MAINNET_RPC_URL, SEPOLIA_RPC_URL, AUCTION_CONTRACT_ADDRESS, USDC_ADDRESS, ETH_ADDRESS, STRK_ADDRESS, LORDS_ADDRESS, SURVIVOR_ADDRESS_MAINNET, BEASTS_NFT_CONTRACT_ADDRESS } from "../lib/constants";
+import {
+  MAINNET_RPC_URL,
+  SEPOLIA_RPC_URL,
+  AUCTION_CONTRACT_ADDRESS,
+  USDC_ADDRESS,
+  ETH_ADDRESS,
+  STRK_ADDRESS,
+  LORDS_ADDRESS,
+  SURVIVOR_ADDRESS_MAINNET,
+  BEASTS_NFT_CONTRACT_ADDRESS,
+} from "../lib/constants";
 
 const provider = jsonRpcProvider({
   rpc: (chain) => {
@@ -23,7 +39,8 @@ const policies = {
   contracts: {
     [AUCTION_CONTRACT_ADDRESS]: {
       namespace: "Survivor Exchange",
-      description: "A place where you can auction your Loot Survivor game monsters",
+      description:
+        "A place where you can auction your Loot Survivor game monsters",
       methods: [
         {
           name: "Create Auction",
@@ -39,8 +56,8 @@ const policies = {
           name: "End Auction",
           description: "End an active auction",
           entrypoint: "end_auction",
-        }
-      ]
+        },
+      ],
     },
     [BEASTS_NFT_CONTRACT_ADDRESS]: {
       namespace: "BEAST NFTs",
@@ -55,91 +72,11 @@ const policies = {
           name: "Transfer",
           description: "Transfer a BEAST NFT",
           entrypoint: "transfer",
-        }
-      ]
-    },
-    [USDC_ADDRESS]: {
-      namespace: "USDC",
-      description: "USD Coin token",
-      methods: [
-        {
-          name: "Approve",
-          description: "Approve a spender to spend your USDC tokens",
-          entrypoint: "approve",
         },
-        {
-          name: "Transfer",
-          description: "Transfer USDC tokens",
-          entrypoint: "transfer",
-        }
-      ]
+      ],
     },
-    [ETH_ADDRESS]: {
-      namespace: "ETH",
-      description: "Ethereum token",
-      methods: [
-        {
-          name: "Approve",
-          description: "Approve a spender to spend your ETH tokens",
-          entrypoint: "approve",
-        },
-        {
-          name: "Transfer",
-          description: "Transfer ETH tokens",
-          entrypoint: "transfer",
-        }
-      ]
-    },
-    [STRK_ADDRESS]: {
-      namespace: "STRK",
-      description: "Starknet token",
-      methods: [
-        {
-          name: "Approve",
-          description: "Approve a spender to spend your STRK tokens",
-          entrypoint: "approve",
-        },
-        {
-          name: "Transfer",
-          description: "Transfer STRK tokens",
-          entrypoint: "transfer",
-        }
-      ]
-    },
-    [LORDS_ADDRESS]: {
-      namespace: "LORDS",
-      description: "Lords token",
-      methods: [
-        {
-          name: "Approve",
-          description: "Approve a spender to spend your LORDS tokens",
-          entrypoint: "approve",
-        },
-        {
-          name: "Transfer",
-          description: "Transfer LORDS tokens",
-          entrypoint: "transfer",
-        }
-      ]
-    },
-    [SURVIVOR_ADDRESS_MAINNET]: {
-      namespace: "Survivor",
-      description: "The native token of the Survivor game",
-      methods: [
-        {
-          name: "Approve",
-          description: "Approve a spender to spend your SURVIVOR tokens",
-          entrypoint: "approve",
-        },
-        {
-          name: "Transfer",
-          description: "Transfer SURVIVOR tokens",
-          entrypoint: "transfer",
-        }
-      ]
-    }
-  }
-}
+  },
+};
 
 const controller = new ControllerConnector({
   policies,
